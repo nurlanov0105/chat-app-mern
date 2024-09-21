1ea037dec435bf9539ce56044459642b578bdda4
Nursultan <nurlanov0105@gmail.com>
2024-09-21 11:05:13 +0600

    fixed deployment error

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,5 @@
 import Message from '../models/message.model.js';
-import Conversation from '../models/Conversation.model.js';
+import Conversation from '../models/conversation.model.js';
 import { getReceiverSocketId, io } from '../socket/socket.js';
 
 export const sendMessage = async (req, res) => {
